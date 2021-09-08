e74db36ae227c8cdccd36074d592ea734b24d03e
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2021-09-08 15:45:29 -0300

    updated Main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,7 +6,7 @@ import "./Main.css"
 function Main() {
   const year = new Date().getFullYear()
   const month = (new Date().getMonth() + 1).toString().padStart(2, "0") 
-  const day = new Date().getDate()
+  const day = new Date().getDate().toString().padStart(2, "0") 
   const dateCurrent= `${day}/${month}/${year}`
   return (
     <main>
